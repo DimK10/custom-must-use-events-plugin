669e16827704b73f500c2e6553a8361711271b7f
DimK10 <james_k23@hotmail.gr>
2021-06-19 13:59:55 +0300

    Lesson 33 finished

--- university-post-types.php
+++ university-post-types.php
@@ -5,6 +5,10 @@
       'public' => true,
       'labels' => array (
         'name' => 'Events',
+        'add_new_item' => 'Add New Event',
+        'edit_item' => 'Edit Event',
+        'all_items' => 'All Events',
+        'singular_name' => 'Event'
       ),
       'menu_icon' => 'dashicons-calendar'
     ));
