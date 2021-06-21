1d5de639284ada354977c64d64fbef8f0b891291
DimK10 <james_k23@hotmail.gr>
2021-06-21 20:02:06 +0300

    Added new editor and excerpts support

--- university-post-types.php
+++ university-post-types.php
@@ -2,7 +2,12 @@
 function university_post_types()
 {
     // public => makes to post type visible to editors and viewers
+    // show_in_rest is for using the modern editor, but, if there is no supports option, or no editor value in supports
+    // then wordpress falls back to the old editor
     register_post_type('event', array(
+        'show_in_rest' => true,
+        'supports' => array('title', 'editor', 'excerpt'),
+        'rewrite' => array('slug' => 'events'),
         'has_archive' => true,
         'public' => true,
         'labels' => array(
