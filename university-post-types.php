cc57bff2f7c82e37b66947356bc847395951750c
DimK10 <james_k23@hotmail.gr>
2021-06-21 19:29:44 +0300

    Added archive support for events

--- university-post-types.php
+++ university-post-types.php
@@ -1,18 +1,20 @@
 <?php
-  function university_post_types() {
+function university_post_types()
+{
     // public => makes to post type visible to editors and viewers
     register_post_type('event', array(
-      'public' => true,
-      'labels' => array (
-        'name' => 'Events',
-        'add_new_item' => 'Add New Event',
-        'edit_item' => 'Edit Event',
-        'all_items' => 'All Events',
-        'singular_name' => 'Event'
-      ),
-      'menu_icon' => 'dashicons-calendar'
+        'has_archive' => true,
+        'public' => true,
+        'labels' => array(
+            'name' => 'Events',
+            'add_new_item' => 'Add New Event',
+            'edit_item' => 'Edit Event',
+            'all_items' => 'All Events',
+            'singular_name' => 'Event'
+        ),
+        'menu_icon' => 'dashicons-calendar'
     ));
-  }
+}
 
-  add_action('init', 'university_post_types');
+add_action('init', 'university_post_types');
 ?>
